0938215e39abe05d46c77aa3d24c68190d8c023a
aewhite568 <aw337@duke.edu>
2019-10-23 13:07:29 -0400

    Add fields to feature class

--- ImportARGOS.py
+++ ImportARGOS.py
@@ -12,6 +12,7 @@
 
 # Import modules
 import sys, os, arcpy
+arcpy.env.overwriteOutput = True
 
 # Set input variables (Hard-wired)
 inputFile = '../Data/ARGOSData/1997dg.txt'
@@ -22,6 +23,11 @@ outputSR = arcpy.SpatialReference(54002) #World equidistant cylindrical (WKID)
 # Create an empty feature class; requires the path and name as separate parameters
 outPath,outName = os.path.split(outputFC)
 arcpy.CreateFeatureclass_management(outPath,outName,"POINT","","","", outputSR)
+
+# Add TagID, LC, IQ, and Date fields to the output feature class
+arcpy.AddField_management(outputFC,"TagID","LONG")
+arcpy.AddField_management(outputFC,"LC","TEXT")
+arcpy.AddField_management(outputFC,"Date","DATE")
 
 ## Construct a while loop to iterate through all lines in the datafile
 # Open the ARGOS data file for reading
